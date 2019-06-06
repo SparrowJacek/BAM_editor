3d59095ce1cf6a948ad089d36f8fcf3b57f62e19
SparrowJacek <jkozmicj@gmail.com>
2019-06-06 21:14:38 +0200

    Moved some logic to kv files and removed dirty way of creating palette

--- BamEditor.py
+++ BamEditor.py
@@ -5,24 +5,17 @@ Config.set('kivy', 'window_icon', r'.\static\program_icon\BamEditor-icon.png')
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 import os
 from kivy.app import App
-from random import random as r
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.label import Label
 from kivy.uix.scatter import Scatter
-from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
-from kivy.graphics import Color, Rectangle
 from kivy.uix.colorpicker import ColorPicker
-from filebrowser import TreeViewBrowser
 from kivy.uix.filechooser import FileChooserIconView, FileChooserListView
 from os.path import expanduser
-from kivy.properties import StringProperty, ListProperty
+from kivy.properties import ListProperty
 from kivy.lang import Builder
 
 Builder.load_file('BamEditor.kv')
@@ -44,10 +37,6 @@ class MainLabel(Label):
     def __init__(self, path=expanduser('~')):
         super(MainLabel, self).__init__()
         self.path = path
-
-
-class ToolBarLabel(Label):
-    pass
 
 
 class NewFileButton(Button):
@@ -193,16 +182,13 @@ class ImageLabel(Label):
     pass
 
 
-class PaletteInfoToolInfoLabel(ToolBarLabel):
-    pass
-
-
-class PaletteLabel(ToolBarLabel):
-    def make_palette(self, layout):
+class ColorButtonsGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         for i in range(0, 256):
             palette_color_button = PaletteColorButton()
             palette_color_button.color = (i / 255, i / 255, i / 255, 1)
-            layout.add_widget(palette_color_button)
+            self.add_widget(palette_color_button)
 
 
 class PaletteColorButton(Button):
@@ -222,10 +208,6 @@ class PaletteColorButton(Button):
                 root.ids['left_mouse_color'].color = self.color
 
 
-class CurrentColorLabel(ToolBarLabel):
-    pass
-
-
 class ColorPickerPopup(Popup):
     def __init__(self, my_widget, **kwargs):  # my_widget is now the object where popup was called from.
         super().__init__(**kwargs)
@@ -235,14 +217,6 @@ class ColorPickerPopup(Popup):
     def dismiss_popup(self, root):
         self.my_widget.color = self.color
         root.ids['left_mouse_color'].color = self.color
-
-
-class ToolInfoLabel(ToolBarLabel):
-    pass
-
-
-class SequenceFrameInfoLabel(ToolBarLabel):
-    pass
 
 
 class SequenceFrameTabbedPanel(TabbedPanel):
@@ -257,8 +231,8 @@ class SequenceLabel(ScrollableLabel):
     pass
 
 
-class SequenceOrderChangeLabel(ToolBarLabel):
-    pass
+# class SequenceOrderChangeLabel(ToolBarLabel):
+#     pass
 
 
 class SequenceUpButton(Button):
@@ -297,8 +271,8 @@ class FrameLabel(ScrollableLabel):
     pass
 
 
-class FrameOrderChangeLabel(ToolBarLabel):
-    pass
+# class FrameOrderChangeLabel(ToolBarLabel):
+#     pass
 
 
 class FrameUpButton(Button):
@@ -333,8 +307,8 @@ class BamEditor(App):
 
     def build(self):
         main_label = MainLabel()
-
-        main_label.ids['palettelabel'].make_palette(main_label.ids['palettelayout'])
+        #
+        # main_label.ids['palettelabel'].make_palette(main_label.ids['palettelayout'])
         return main_label
 
 
