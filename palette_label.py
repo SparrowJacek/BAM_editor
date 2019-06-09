a779088315d27f0e48c115624c6386a1f7abd11b
SparrowJacek <jkozmicj@gmail.com>
2019-06-09 11:53:20 +0200

    Fixed bug with double click on palette buttons

--- palette_label.py
+++ palette_label.py
@@ -19,16 +19,21 @@ class PaletteColorButton(Button):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.current_touch = None
+        self.scheduled_action = None
+
+    def reset_action_params(self):
+        self.current_touch = None
+        self.scheduled_action = None
 
     def touch_down_actions(self, touch, root):
         if self.collide_point(*touch.pos):
             if self.current_touch is not None:
-                Clock.unschedule(self.on_single_press)
                 self.on_double_press()
-                self.current_touch = None
+                self.reset_action_params()
             else:
                 self.current_touch = touch
-                Clock.schedule_once(lambda dt: self.on_single_press(touch, root), 0.1)
+                self.scheduled_action = Clock.schedule_once(lambda dt: self.on_single_press(touch, root),
+                                                            0.2)
 
     def on_double_press(self):
         color_picker_popup = ColorPickerPopup(self)
@@ -39,6 +44,7 @@ class PaletteColorButton(Button):
             root.ids['right_mouse_color'].color = self.color
         if touch.button == "left":
             root.ids['left_mouse_color'].color = self.color
+        self.reset_action_params()
 
     def pick_color(self, *args):
         print(self.pos)
