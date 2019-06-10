10e973089cac14ab640bb13f048fed3d6be6bd9c
SparrowJacek <jkozmicj@gmail.com>
2019-06-10 11:40:20 +0200

    Added dynamic color but palette label is created 2 times

--- palette_label.py
+++ palette_label.py
@@ -1,4 +1,6 @@
+from kivy.app import App
 from kivy.clock import Clock
+from kivy.input.providers.wm_touch import WM_MotionEvent
 from kivy.properties import ListProperty
 from kivy.uix.button import Button
 from kivy.uix.colorpicker import ColorPicker
@@ -8,6 +10,7 @@ from kivy.uix.popup import Popup
 
 class ColorButtonsGridLayout(GridLayout):
     def __init__(self, **kwargs):
+        print('yup')
         super().__init__(**kwargs)
         for i in range(0, 256):
             palette_color_button = PaletteColorButton()
@@ -25,41 +28,63 @@ class PaletteColorButton(Button):
         self.current_touch = None
         self.scheduled_action = None
 
-    def touch_down_actions(self, touch, root):
+    def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
+            print(self.color)
             if self.current_touch is not None:
                 self.on_double_press()
-                self.reset_action_params()
             else:
                 self.current_touch = touch
-                self.scheduled_action = Clock.schedule_once(lambda dt: self.on_single_press(touch, root),
+                self.scheduled_action = Clock.schedule_once(lambda dt: self.on_single_press(touch),
                                                             0.2)
 
     def on_double_press(self):
         color_picker_popup = ColorPickerPopup()
-        color_picker = MyColorPicker(self)
+        color_picker = MyColorPicker(color_picker_popup, self)
         color_picker_popup.add_widget(color_picker)
         color_picker_popup.open()
 
-    def on_single_press(self, touch, root):
+
+    def on_single_press(self, touch):
+        try:
+            self.set_mouse_color(touch)
+        except AttributeError:
+            pass
+        finally:
+            self.reset_action_params()
+
+    def set_mouse_color(self, touch):
+        app = App.get_running_app()
+        root_label = app.root
         if touch.button == "right":
-            root.ids['right_mouse_color'].color = self.color
+            root_label.ids['right_mouse_color'].color = self.color
         if touch.button == "left":
-            root.ids['left_mouse_color'].color = self.color
-        self.reset_action_params()
+            root_label.ids['left_mouse_color'].color = self.color
 
     def pick_color(self, *args):
         print(self.pos)
 
 
 class ColorPickerPopup(Popup):
-    def dismiss_popup(self, root):
-        self.my_widget.color = self.color
-        root.ids['left_mouse_color'].color = self.color
+    pass
 
 
 class MyColorPicker(ColorPicker):
-    def __init__(self, linked_button, **kwargs):
-        super().__init__(**kwargs)
-        self.linked_button = linked_button
+    def __init__(self, linked_popup, linked_button, **kwargs):
         self.color = linked_button.color
+        super().__init__(**kwargs)
+        self.linked_popup = linked_popup
+        self.linked_button = linked_button
+
+
+class MyColorPickerButton(Button):
+    def get_popup(self):
+        parent = self.parent
+        while True:
+            if issubclass(type(parent), Popup):
+                return parent
+            parent = parent.parent
+
+    def on_press(self):
+        parent_popup = self.get_popup()
+        parent_popup.dismiss()
